e93ed71b9cfb5977d3ec7144f03d608748ea4419
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2018-05-09 15:46:23 -0700

    Fix the reading of config files for postgres

--- backend/fconfig/config.go
+++ backend/fconfig/config.go
@@ -1,25 +1,47 @@
 package fconfig
 
 import (
-	"github.com/pelletier/go-toml"
+	"github.com/BurntSushi/toml"
+	"io/ioutil"
 	"log"
 	"time"
 )
 
+type Postgres struct {
+	Host     string
+	User     string
+	Port     int64
+	Password string
+	Database string
+	SSLmode  string
+}
+
 type Config struct {
-	SpannerDatabase string
-	FBinSize        time.Duration
+	Postgres Postgres
+	FBinSize time.Duration
+}
+
+type duration struct {
+	time.Duration
+}
+
+func (d *duration) UnmarshalText(text []byte) error {
+	var err error
+	d.Duration, err = time.ParseDuration(string(text))
+	return err
 }
 
 func ReadConfiguration(path string) Config {
 	cfg := Config{}
-	tt, err := toml.LoadFile(path)
+	configText, err := ioutil.ReadFile(path)
 
 	if err != nil {
-		log.Fatalf("configuration error: %v", err)
+		log.Fatalf("failed to read configuration file (%s): %v", path, err)
 	}
 
-	tt.Unmarshal(&cfg)
+	if _, err = toml.Decode(string(configText), &cfg); err != nil {
+		log.Fatalf("configuration error (%s): %v", path, err)
+	}
 
 	return cfg
 }
--- backend/manager/manager.go
+++ backend/manager/manager.go
@@ -3,12 +3,32 @@ package manager
 import (
 	"flights/fconfig"
 	"flights/spapi"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq" // postgres driver
 	"golang.org/x/net/context"
+	"log"
 	"time"
 )
 
 func MakeCache(config fconfig.Config) (*spapi.Cache, error) {
 	ctx, _ := context.WithTimeout(context.Background(), 20*time.Hour)
 
-	return spapi.NewCache(ctx, config.SpannerDatabase, config.FBinSize)
+	connStr := fmt.Sprintf("user=%s password=%s host=%s port=%d dbname=%s sslmode=%s",
+		config.Postgres.User,
+		config.Postgres.Password,
+		config.Postgres.Host,
+		config.Postgres.Port,
+		config.Postgres.Database,
+		config.Postgres.SSLmode)
+
+	log.Printf("Connecting to postgres with: %s\n", connStr)
+
+	db, err := sqlx.Connect("postgres", connStr)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return spapi.NewCache(ctx, db, config.FBinSize)
 }
