446f44ca0cd25820e3c9f5e0302e291bed2db99e
Alexander Shearer <acshear1@asu.edu>
2018-05-14 20:24:08 -0700

    fixed post for realz this time

--- hoboapp/lib/main.dart
+++ hoboapp/lib/main.dart
@@ -15,7 +15,8 @@ Map<String, String> _nameToCode = new Map<String, String>();
 List<String> _currentAirportCodes = [];
 PostData postData = new PostData();
 Fuzzy _fuzz;
-String _url = 'http://requestbin.fullcontact.com/1bnkxwj1';
+String _url = 'http://142.4.213.30:8080/search';
+//String _url = 'http://requestbin.fullcontact.com/1401f421';
 
 void main() async {
   _init();
@@ -188,6 +189,7 @@ class _MyHomePageState extends State<MyHomePage>
             child: new Icon(IconData(0xe409,
                 fontFamily: 'MaterialIcons', matchTextDirection: true)),
             onPressed: () {
+              _currentAirportCodes = [];
               for (int i = 0; i < _selectedList.length; i++) {
                 _currentAirportCodes.add(_nameToCode[_selectedList[i]]);
                 if (_currentAirportCodes[i] == null) {
@@ -232,7 +234,7 @@ class _ParameterScreenState extends State<ParameterScreen> {
         onChanged: (String newVal) {
           if (tif == oneLineNumbers) {
             int val = int.tryParse(newVal);
-            switch(hint) {
+            switch (hint) {
               case 'Max Stay':
                 postData.maxStay = val;
                 break;
@@ -291,14 +293,17 @@ class _ParameterScreenState extends State<ParameterScreen> {
         _currentAirportCodes.getRange(1, _currentAirportCodes.length).toList();
 
     print(postData.googleCantJSONThingsSoIWillDoIt());
-    sub.post(postData.googleCantJSONThingsSoIWillDoIt(), _url);
-    //TODO: use futures or something here
-    while(!sub.done){};
-    if(sub.statusCode == 200){
-      ResultScreen.token = sub.body;
-    } else {
-      ResultScreen.token = 'hi';
-    }
+    sub.post(postData.googleCantJSONThingsSoIWillDoIt(), _url).then((Response response) {
+      print(response.statusCode);
+      print(response.responseBody);
+      if (response.statusCode == 200) {
+        ResultScreen.token = response.responseBody;
+      } else {
+        //SHOW ERROR
+        ResultScreen.token = 'hi';
+      }
+    });
+    //HttpClientResponse = await sub.post2(googleCantJSONThingsSoIWillDoIt(), _url, '8080', '/search');
   }
 
   List<TextInputFormatter> oneLineNumbers = [
