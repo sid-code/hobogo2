61364254c711a3388e10fc340e52575440919668
Alexander Shearer <acshear1@asu.edu>
2018-05-20 23:10:03 -0700

    Websockets now display first flight in array

--- hoboapp/lib/results.dart
+++ hoboapp/lib/results.dart
@@ -19,8 +19,8 @@ class ResultScreen extends StatefulWidget {
 
 class _ResultScreenState extends State<ResultScreen> {
   IOWebSocketChannel ws;
+  WebSocket ws2;
   bool firstRun = true;
-
   Card _genResult(Flight input) {
     Flight inFlight = input;
     return new Card(
@@ -54,24 +54,18 @@ class _ResultScreenState extends State<ResultScreen> {
               new Align(
                 widthFactor: 1.5,
                 alignment: Alignment.center,
-                child:
-                new Text(
-                    new DateFormat.yMd().format(inFlight.DepartTime) + '\n' +
-                    new DateFormat.jm().format(inFlight.DepartTime),
+                child: new Text(
+                    new DateFormat.yMd().format(inFlight.DepartTime) +
+                        '\n' +
+                        new DateFormat.jm().format(inFlight.DepartTime),
                     textAlign: TextAlign.left),
               ),
               new Align(
                 widthFactor: 1.5,
                 alignment: Alignment.center,
-                child: new Text(
-                    inFlight.From,
-                    textAlign: TextAlign.center
-                ),
+                child: new Text(inFlight.From, textAlign: TextAlign.center),
               ),
-              new Icon(
-                IconData(0xe5c8, fontFamily: 'MaterialIcons')
-              ),
-
+              new Icon(IconData(0xe5c8, fontFamily: 'MaterialIcons')),
               new Align(
                 widthFactor: 1.5,
                 alignment: Alignment.center,
@@ -83,9 +77,9 @@ class _ResultScreenState extends State<ResultScreen> {
               new Align(
                 widthFactor: 1.5,
                 alignment: Alignment.center,
-                child:
-                new Text(
-                    new DateFormat.yMd().format(inFlight.ArriveTime) + '\n' +
+                child: new Text(
+                    new DateFormat.yMd().format(inFlight.ArriveTime) +
+                        '\n' +
                         new DateFormat.jm().format(inFlight.ArriveTime),
                     textAlign: TextAlign.right),
               ),
@@ -93,59 +87,65 @@ class _ResultScreenState extends State<ResultScreen> {
                 IconData(0xe192, fontFamily: 'MaterialIcons'),
               ),
               new Align(
-                alignment: Alignment.centerRight,
-                  child: new Text(inFlight.TravelTime.inHours.toString() +
-                      'h ' +
-                      (inFlight.TravelTime.inMinutes -
-                              60 * inFlight.TravelTime.inHours)
-                          .toString() +
-                      'm',
+                  alignment: Alignment.centerRight,
+                  child: new Text(
+                    inFlight.TravelTime.inHours.toString() +
+                        'h ' +
+                        (inFlight.TravelTime.inMinutes -
+                                60 * inFlight.TravelTime.inHours)
+                            .toString() +
+                        'm',
                     textAlign: TextAlign.left,
-                  )
-              )
+                  ))
             ],
           ),
         ]));
   }
 
-  static DateTime timeD = new DateTime.utc(2018,5,20,2,30);
-  static DateTime timeA = new DateTime.utc(2018,5,20,10,30);
-
-  Flight ftest = new Flight(
-      1, 'LGW', 'LAX', timeD,
-      timeA, 800.0, 'testLink', 3);
-
   @override
   Widget build(BuildContext context) {
     if (firstRun) {
       if (!ResultScreen.token.isEmpty) {
-        ws = new IOWebSocketChannel.connect(
-            'ws://142.4.213.30:8080/subscribe?token=' + ResultScreen.token);
+        WebSocket
+            .connect(
+                'ws://142.4.213.30:8080/subscribe?token=' + ResultScreen.token)
+            .then((WebSocket socket) {
+          ws2 = socket;
+        });
       }
       firstRun = !firstRun;
     }
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Results'),
-        //TODO: don't connect with invalid token
       ),
       body: new Center(
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            new Expanded(
-            child: _genResult(ftest),
-            ),
             new Text(ResultScreen.token),
-            /*new StreamBuilder(
-              stream: ws.stream,
+            new Flexible(
+                child: new StreamBuilder(
+              stream: ws2,
               builder: (context, snapshot) {
-                return new Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 24.0),
-                  child: new Text(snapshot.hasData ? '${snapshot.data}' : ''),
-                );
+                print(snapshot.data);
+                if (snapshot.data != null && snapshot.data.length > 0) {
+                  dynamic data = JSON.decode(snapshot.data);
+                  Flight f = new Flight(
+                      int.parse(data[0]['id'].toString()),
+                      data[0]['loc'],
+                      data[0]['from'].toString(),
+                      int.parse(data[0]['departtime'].toString()),
+                      int.parse(data[0]['arrivetime'].toString()),
+                      double.parse(data[0]['price'].toString()),
+                      data[0]['deeplink'].toString(),
+                      int.parse(data[0]['passengers'].toString()));
+                  return _genResult(f);
+                } else {
+                  return new Text('nothing here');
+                }
               },
-            )*/
+            )),
           ],
         ),
       ),
@@ -164,16 +164,16 @@ class Flight {
   int Passengers;
   Duration TravelTime;
 
-  Flight(int id, String loc, String from, DateTime departtime,
-      DateTime arrivetime, double price, String deeplink, int passengers) {
+  Flight(int id, String loc, String from, int departtime, int arrivetime,
+      double price, String deeplink, int passengers) {
     this.ID = ID;
     this.Loc = loc;
     this.From = from;
-    this.DepartTime = departtime;
-    this.ArriveTime = arrivetime;
+    this.DepartTime = new DateTime(departtime);
+    this.ArriveTime = new DateTime(arrivetime);
     this.Price = price;
     this.DeepLink = deeplink;
     this.Passengers = passengers;
-    this.TravelTime = arrivetime.difference(departtime);
+    this.TravelTime = this.ArriveTime.difference(this.DepartTime);
   }
 }
