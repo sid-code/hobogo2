8c926da3f6772e4638acc2fc4737edfd1f365859
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2018-05-20 20:16:45 -0700

    Remove need for context in manager

--- backend/manager/manager.go
+++ backend/manager/manager.go
@@ -6,21 +6,26 @@ import (
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq" // postgres driver
-	"golang.org/x/net/context"
 	"log"
-	"time"
+	"os"
 )
 
 func MakeCache(config fconfig.Config) (*spapi.Cache, error) {
-	ctx, _ := context.WithTimeout(context.Background(), 20*time.Hour)
+	var connStr string
 
-	connStr := fmt.Sprintf("user=%s password=%s host=%s port=%d dbname=%s sslmode=%s",
-		config.Postgres.User,
-		config.Postgres.Password,
-		config.Postgres.Host,
-		config.Postgres.Port,
-		config.Postgres.Database,
-		config.Postgres.SSLmode)
+	connFromEnv := os.Getenv("POSTGRES_CONNECTION")
+	if connFromEnv == "" {
+		connStr = fmt.Sprintf(
+			"user=%s password=%s host=%s port=%d dbname=%s sslmode=%s",
+			config.Postgres.User,
+			config.Postgres.Password,
+			config.Postgres.Host,
+			config.Postgres.Port,
+			config.Postgres.Database,
+			config.Postgres.SSLmode)
+	} else {
+		connStr = connFromEnv
+	}
 
 	log.Printf("Connecting to postgres with: %s\n", connStr)
 
